cd2e2ba6ae196ae37d638e16fe5022c983dd2180
davidbaronvega <141321155+davidbaronvega@users.noreply.github.com>
2024-03-29 01:30:38 -0400

    3/28-3/29
    Base Branch that contains all tested functionality.

---
+++ UnityVR.1/Assets/myassets/locoMove.cs
@@ -0,0 +1,22 @@
+﻿//David Baron-Vega - GFF7068 - 3.29
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class BasicLocomotion : MonoBehaviour
+{
+    public SteamVR_Action_Vector2 moveAction;
+    public float speed = 3.0f;
+
+    void Update()
+    {
+        Vector2 trackpadValue = moveAction.GetAxis(SteamVR_Input_Sources.LeftHand);
+        Vector3 moveDirection = new Vector3(trackpadValue.x, 0, trackpadValue.y);
+        
+        // Convert moveDirection from local to world space if necessary
+        moveDirection = transform.TransformDirection(moveDirection);
+        transform.position += moveDirection * speed * Time.deltaTime;
+    }
+}
